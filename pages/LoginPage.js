710557a5f7d3370db9bc3ab6ce285013ed230c0d
anjaligrapecity_1 <anjali.chander@grapecity.com>
2025-03-25 09:53:38 +0530

    valid login script updated

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -7,10 +7,13 @@ class LoginPage {
       this.passwordInput = 'input[name="password"]';
       this.signInButton = 'input[type="submit"]';
       this.errorMessage = 'xpath=//div[contains(@class, "okta-form-infobox-error")]//p';
+      this.validUsername = "input[name='username']";
+      this.validPassword = "input[name='password']";
+      this.signInButtonValid = "button[type='submit']";
     }
   
     async goto() {
-      await this.page.goto('https://qa-app-01.qventus.com');
+      await this.page.goto('https://qa-app-06.qventus.com');
     }
   
     async enterUsername(username) {
@@ -39,6 +42,18 @@ class LoginPage {
         await this.clickNext();
         await this.enterPassword(password);
         await this.clickSignIn();
+    }
+
+    async enterValidUsername(username) {
+      await this.page.fill(this.validUsername, username);
+    }
+  
+    async enterValidPassword(password) {
+      await this.page.fill(this.validPassword, password);
+    }
+
+    async clickLoginButton() {
+      await this.page.click(this.signInButtonValid);
     }
   }
   
--- tests/badLogin.spec.js
+++ tests/badLogin.spec.js
@@ -1,6 +1,25 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 import LoginPage from '../pages/LoginPage';
+
+/*
+URL == https://qa-app-06.qventus.com	
+Login with hjayaswal+2000@qventus.com  
+and T@c301124
+
+Himanshu Jayaswal
+19:22
+7.	Locate Timefinder icon and click 
+8.	Enter patient name – Test Auto
+9.	Select “Charles” as surgeon
+10.	Select 30 days duration from today
+11.	Select 3rd Procedure 
+12.	Select first service
+13.	Click Search Icon
+locate element and create
+
+*/
+
 
 // Test for login with invalid credentials
 test('Login with bad username', async ({ page }) => {
@@ -14,11 +33,39 @@ test('Login with bad username', async ({ page }) => {
   await loginPage.assertErrorMessage('Unable to sign in');
 });
 
-// test('Login with valid credentials', async ({ page }) => {
-//   const loginPage = new LoginPage(page);
-//   await loginPage.goto();
-//   await loginPage.enterUsername('');
-//   await loginPage.enterPassword('');
-//   await loginPage.clickSignIn();
-//   await expect(page).toHaveURL();
-// });
+test('Login with valid credentials', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  test.setTimeout(120000);
+  await loginPage.goto();
+  await loginPage.enterValidUsername('hjayaswal+2000@qventus.com');
+  await loginPage.enterValidPassword('T@c301124');
+  await loginPage.clickLoginButton();
+  await expect(page).toHaveURL("https://qa-app-06.qventus.com/");
+
+await page.locator("text=TimeFinder").nth(0).click();
+await page.waitForLoadState('load'); 
+
+await expect(page.locator("#case_name")).toBeVisible();
+await page.fill("#case_name", "automation");
+
+//await page.locator("#procedures-38_procedure_name").click();
+//await page.locator("//li[@data-surgeon-id='5']").click();
+
+await page.locator("input[placeholder='Start date']").click();
+await page.locator("button[aria-label='calendar-day-24']").first().click();
+await page.locator("button[aria-label='calendar-day-24']").nth(1).click();
+
+await page.locator("input[aria-label*='procedure']").press('enter');
+await page.locator("li[data-procedure-id='3148']").click();
+
+await page.locator("input[id='procedures-38_case_specialty']").click();
+await page.locator("div[id='20']").click();
+
+await page.locator("#duration").fill('20');
+
+await page.locator("button[data-testid='slotsearchform-submit']").click();
+
+// Add assertion to verify successful form submission
+await expect(page.locator("selector-for-success-message")).toHaveText("Expected Success Message");
+
+});
